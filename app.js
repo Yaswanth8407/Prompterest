d0ec7fb9d6203dbe7282d417294160d3e23cf7e6
Mattaparthi Yaswanth <mattaparthiyaswanth8407@gmail.com>
2026-05-17 14:29:40 +0530

    feat: initialize project with package.json and landing page template
    - Added package.json with dependencies and scripts for the project.
    - Created landing.ejs file with basic HTML structure and a greeting message.

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(import.meta.dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("landing");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
