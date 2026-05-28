543d461a1d71267a4081e63df26a235754be18d7
Mattaparthi Yaswanth <mattaparthiyaswanth8407@gmail.com>
2026-05-28 15:16:14 +0530

    Add user model schema with validation and timestamps

---
+++ models/userModel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/],
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      select: false,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("User", userSchema);
