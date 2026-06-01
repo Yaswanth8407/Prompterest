4cb7a342b5edaa75e1563305c5ff99debd7c2b60
Mattaparthi Yaswanth <mattaparthiyaswanth8407@gmail.com>
2026-06-01 23:25:44 +0530

    refactor: remove unused cookie-parser import from app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import path from "path";
 import user from "./models/userModel.js";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
-// import cookieParser from "cookie-parser";
 
 const app = express();
 const port = 3000;
