575aaef0caad509abbd96a305bd023d839a74c53
Mattaparthi Yaswanth <mattaparthiyaswanth8407@gmail.com>
2026-06-02 22:32:35 +0530

    feat: add JWT authentication and cookie handling to signup process

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ import path from "path";
 import user from "./models/userModel.js";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
-// import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
 
 const app = express();
 const port = 3000;
@@ -12,6 +13,7 @@ const port = 3000;
 dotenv.config();
 
 app.set("view engine", "ejs");
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(import.meta.dirname, "public")));
@@ -52,6 +54,24 @@ app.post("/signup", async (req, res) => {
       username,
       email,
       password: hashedPass,
+    });
+
+    const token = jwt.sign(
+      {
+        username,
+        email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      },
+    );
+
+    res.cookie("PrompterestAuthToken", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 604800000,
     });
 
     res.redirect("/feed");
