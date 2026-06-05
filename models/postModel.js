c60ef7b47649b2c9711d5973047b1cea290916df
Mattaparthi Yaswanth <mattaparthiyaswanth8407@gmail.com>
2026-06-05 09:39:34 +0530

    feat: define post schema with required fields for title, prompt, aiTool, and category

--- models/postModel.js
+++ models/postModel.js
@@ -1,8 +1,27 @@
 import mongoose from "mongoose";
 
 const postSchema = mongoose.Schema({
-
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  desc: {
+    type: String,
+  },
+  prompt: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  aiTool: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
 });
 
 export default mongoose.model("Post", postSchema);
-// hsj
