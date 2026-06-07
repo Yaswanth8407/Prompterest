b7711ec9175088537c0e93737865f17e7067cc21
Mattaparthi Yaswanth <mattaparthiyaswanth8407@gmail.com>
2026-06-07 20:40:14 +0530

    fix: correct upload destination path in multer configuration

--- config/multerconfig.js
+++ config/multerconfig.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads");
+    cb(null, "../public/uploads");
   },
   filename: function (req, file, cb) {
     const Finalfilename =
