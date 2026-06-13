1b2a8b20815e7e4d6e950ba0f59bddfa80690dce
Mattaparthi Yaswanth <mattaparthiyaswanth8407@gmail.com>
2026-06-14 00:30:28 +0530

    fix: update multer configuration to use async filename function and handle user profile picture updates

--- config/multerconfig.js
+++ config/multerconfig.js
@@ -2,16 +2,22 @@ import multer from "multer";
 import path from "path";
 import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import user from "../models/userModel.js";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, path.join(import.meta.dirname,"../public/uploads/profilepics"));
   },
-  filename: function (req, file, cb) {
-    const Finalfilename =
-      jwt.verify(req.cookies.PrompterestAuthToken, process.env.JWT_SECRET)
-        .username +
+  filename: async function (req, file, cb) {
+    const foundCredentials = jwt.verify(req.cookies.PrompterestAuthToken, process.env.JWT_SECRET)
+    
+    const Finalfilename = foundCredentials.username +
       path.extname(file.originalname);
+      
+    await user.findOneAndUpdate({username: foundCredentials.username},{
+      profilepic: "uploads/profilepics/" + Finalfilename,
+    })
     cb(null, Finalfilename);
   },
 });
