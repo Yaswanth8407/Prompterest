a0c3541c6bdad915e6c08c15ea1eca585d914a46
Mattaparthi Yaswanth <mattaparthiyaswanth8407@gmail.com>
2026-06-17 20:56:06 +0530

    fix: switch multer storage to memory storage for profile picture uploads

--- config/multerconfig.js
+++ config/multerconfig.js
@@ -1,27 +1,32 @@
 import multer from "multer";
 import path from "path";
 import jwt from "jsonwebtoken";
-import cookieParser from "cookie-parser";
-import mongoose from "mongoose";
 import user from "../models/userModel.js";
+import sharp from "sharp";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(import.meta.dirname,"../public/uploads/profilepics"));
-  },
-  filename: async function (req, file, cb) {
-    const foundCredentials = jwt.verify(req.cookies.PrompterestAuthToken, process.env.JWT_SECRET)
-    
-    const Finalfilename = foundCredentials.username +
-      path.extname(file.originalname);
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, path.join(import.meta.dirname,"../public/uploads/profilepics"));
+//   },
+//   filename: async function (req, file, cb) {
+//     const foundCredentials = jwt.verify(req.cookies.PrompterestAuthToken, process.env.JWT_SECRET)
+
+//     sharp(foundCredentials.username +
+//       path.extname(file.originalname))
       
-    await user.findOneAndUpdate({username: foundCredentials.username},{
-      profilepic: "uploads/profilepics/" + Finalfilename,
-    })
-    cb(null, Finalfilename);
-  },
-});
+//     const Finalfilename = foundCredentials.username +
+//       path.extname(file.originalname);
+      
+//     await user.findOneAndUpdate({username: foundCredentials.username},{
+//       profilepic: "uploads/profilepics/" + Finalfilename,
+//     })
+//     cb(null, Finalfilename);
+//   },
+// });
 
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
 
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 export default upload
